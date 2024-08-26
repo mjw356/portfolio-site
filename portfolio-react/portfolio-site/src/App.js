594bda2e6b08baef48924e94e25f44efdeaeb65e
Michael <wolf.michael.j@gmail.com>
2024-08-26 13:54:26 -0400

    added code to create the homepage

--- portfolio-react/portfolio-site/src/App.js
+++ portfolio-react/portfolio-site/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Container>
+        <Row>
+          <Col md={6}>
+            <h1>Wolf On The Web</h1>
+          </Col>
+          <Col md={6}>
+            <Image src="roastingmarshmallows.webp" fluid></Image>
+          </Col>
+        </Row>
+      </Container>
       </header>
     </div>
   );
