d28cf2f6938b71f3cdce4f925f64b1eb190ebfb9
Michael <wolf.michael.j@gmail.com>
2024-09-09 09:08:17 -0400

    changed secret key

--- portfolioDjango/portfolioDjango/settings.py
+++ portfolioDjango/portfolioDjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!v(t4*)wszf3e8tpr5zn!use02gt$+b+qf)gn*kgnlg5phx4b7'
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY=f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
